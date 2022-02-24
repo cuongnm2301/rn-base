1045073cd7244d1fc63448389fa6378310c6dfbe
cuongnm <cuong.nguyen@amela.vn>
2022-02-24 23:13:37 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const App = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Welcome</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
+
+export default App;
