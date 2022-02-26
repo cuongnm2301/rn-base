c839dad90a70e8ac586436acda40a58e4c119886
thuctv115@gmail.com <thuc>
2022-02-26 23:02:06 +0700

    feature/addCodePush

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,25 @@ import Home from 'features/home/Home';
 import { Provider } from 'react-redux';
 import { persistor, store } from 'redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
-import { ActivityIndicator } from 'react-native';
+import { ActivityIndicator, Platform } from 'react-native';
+import Config from 'react-native-config';
+import codePush from 'react-native-code-push';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+    React.useEffect(() => {
+        if (!__DEV__) {
+            codePush.sync({
+                updateDialog: undefined,
+                installMode: codePush.InstallMode.IMMEDIATE,
+                deploymentKey:
+                    Platform.OS === 'android'
+                        ? Config.CODEPUSH_ANDROID_DEVELOPMENT_KEY
+                        : Config.CODEPUSH_IOS_DEVELOPMENT_KEY,
+            });
+        }
+    }, []);
     return (
         <Provider store={store}>
             <PersistGate loading={<ActivityIndicator />} persistor={persistor}>
